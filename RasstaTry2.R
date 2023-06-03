5507a0c45e367fac9962f794fd6771fef75b4920
LabComputerDownstairs <e.s.klaussner@sms.ed.ac.uk>
2023-06-03 17:12:33 +0100

    Binarization of the Landcover classes
    Split up the landcover classes in QGIS and binarized each individually.

--- RasstaTry2.R
+++ RasstaTry2.R
@@ -1,4 +1,4 @@
-setwd("M:/dissertation/RAllTries")
+setwd("M:/dissertation/DissertationThings")
 library(terra)
 library(mclust)
 library(sf)
@@ -9,7 +9,7 @@ library(rgdal)
 library(fpc)
 library(maptools)
 library(mapview)
-#library(spatialEco)
+#library(spatialEco)ss
 library(biclust)
 #library(SpatialPack)
 library(ggmap)
@@ -20,44 +20,50 @@ library(rassta)
 #using the terra package to load in the raster datasets
 eleva.tif <- rast("data/elevation.tif")
 pop.tif <- rast("data/ken_general_2020.tif")
-lc.tif <- rast("data/PROBAV_LC100_global_v3.0.1_2019-nrt_Discrete-Classification-map_EPSG-4326.tif")
-lc.bare.tif <- rast("data/fractional_cover_lc100/PROBAV_LC100_global_v3.0.1_2019-nrt_Bare-CoverFraction-layer_EPSG-4326.tif")
 
-#Create new NA data
+#Different Landcover Binary Values
+lc.bare.tif <- rast("data/fractional_cover_lc100/Bare-SparseVegetation.tif")
+lc.forst <- rast("data/fractional_cover_lc100/AllForestTypes.tif")
+lc.crop <- rast("data/fractional_cover_lc100/Cropland.tif")
+lc.urban <- rast("data/fractional_cover_lc100/UrbanBuiltUp.tif")
+lc.water <- rast("data/fractional_cover_lc100/Water.tif")
+lc.shrubs <- rast("data/fractional_cover_lc100/Shrubs.tif")
+lc.herb.veg <- rast("data/fractional_cover_lc100/HerbaceousVegetation.tif")
+lc.herb.wet <- rast("data/fractional_cover_lc100/HerbaceousWetland.tif")
+
+#Combine the datasets to a stack with different layers
+lc.stack <- (c(lc.bare.tif, lc.forst, lc.crop, lc.urban, lc.water, lc.shrubs, lc.herb.veg, lc.herb.wet))
 
 #read in the rectangle
 rectangle <-st_read("data/rectangle.shp")
 
 #crop to extent of the rectangle (to test out the code so far):
 eleva.rec.tif <- terra::crop(eleva.tif, rectangle)
-#eleva.rec.tif <- mask(eleva.tif2, rectangle)
-
 pop.rec.tif <- crop(pop.tif, rectangle)
-#pop.rec.tif <- mask(pop.tif2, rectangle)
 
-lc.rec.tif <- crop(lc.tif, rectangle)
-lc.bare.rec.tif <- crop(lc.bare.tif, rectangle)
+#Crop the stack to the extent of the rectangle
+lc.rec.stack <- crop(lc.stack, rectangle)
 
-new_value <- 999 
+#Create new NA data (assuming the population in unknown is 0)
+new_value <- 0 
 pop.rec.tif[is.na(pop.rec.tif)] <- new_value
-lc.rec.tif[is.na(lc.rec.tif)] <- new_value
-lc.bare.rec.tif[is.na(lc.bare.rec.tif)] <- new_value
 
-#resample data so I can combine them (this leads to aggregation issues!)
+#resample data so I can combine them (this leads to aggregation issues!) CHECK AGAIN WHETHER THAT IS NEEDED
 resamp.pop<- resample(pop.rec.tif, eleva.rec.tif)
 resamp.lc <- resample(lc.rec.tif, eleva.rec.tif)
 resamp.lc.bare <- resample(lc.bare.rec.tif, eleva.rec.tif)
 
-#omit NAs
-foc.lc <- focal(resamp.lc)
-foc.pop<- focal(resamp.pop)
-foc.bare.lc <- focal(resamp.lc.bare)
+#omit NAs (Also check whether still needed, except for elevation)
+#foc.lc <- focal(resamp.lc)
+#foc.pop<- focal(resamp.pop)
+#foc.bare.lc <- focal(resamp.lc.bare)
 foc.ele <- focal(eleva.rec.tif)
 
 #library(raster)
-stack1 <- (c(foc.ele, foc.pop, foc.bare.lc, foc.lc))
-#rename the coloumns so that I can identify them
-names(stack1) <- c("elevation", "population", "bare", "landcover")
+stack1 <- (c(foc.ele, pop.rec.tif, lc.rec.stack))
+
+#rename the coloumns so that I can identify them, need to check how to rename them again, and where it went wrong
+#names(stack1) <- c("elevation", "population", "bare", "landcover")
 
 #supersom with Rassta pray for me
 set.seed(9)
